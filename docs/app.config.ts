6482d5e3b702c16db355c2bce0785f8e484d479a
Giovane Cardoso <novout@hotmail.com>
2023-01-04 15:01:36 -0300

    docs: initial docus implementation

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -2,7 +2,6 @@ export default defineAppConfig({
   docus: {
     title: 'UnunuraCSS',
     description: 'Simplified On-Demand Atomic CSS Engine.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     aside: {
       level: 0,
       exclude: [],
