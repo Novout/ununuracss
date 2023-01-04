80385eb3132e6ba5155e24abbb1f9048ae234df8
Giovane Cardoso <novout@hotmail.com>
2023-01-04 17:04:56 -0300

    fix(vite): reload only in reset timers

--- packages/vite/src/hot.ts
+++ packages/vite/src/hot.ts
@@ -2,7 +2,7 @@ import { RESOLVED_VIRTUAL_CSS_INJECT_FILENAME } from 'ununura-shared'
 import type { ViteDevServer } from 'vite'
 
 export const reloadServer = async (server: ViteDevServer) => {
-  const virtualModule = server.moduleGraph.getModuleById(RESOLVED_VIRTUAL_CSS_INJECT_FILENAME)!
+  const virtualModule = server.moduleGraph.getModuleById(RESOLVED_VIRTUAL_CSS_INJECT_FILENAME)
 
-  await server.reloadModule(virtualModule)
+  if (virtualModule) await server.reloadModule(virtualModule)
 }
