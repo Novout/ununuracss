fee0eaa370ca8617fdbb6296fda5ef597e116fbf
Novout <novout@hotmail.com>
2023-01-27 01:40:21 +0000

    chore(vite): satisfies operator in options

--- packages/vite/src/options.ts
+++ packages/vite/src/options.ts
@@ -1,17 +1,16 @@
 import { UnunuraOptions, UnunuraResolvableOptions } from 'ununura-shared'
 
 export const resolvedViteOptions = (options: UnunuraResolvableOptions = {}): UnunuraOptions => {
-  let _options = {} as UnunuraOptions
-  _options.defines = options?.defines ?? []
-  _options.jsx = options?.jsx ?? false
-  _options.jsxIgnoreEntryFile = options?.jsxIgnoreEntryFile ?? true
-  _options.scopedInTemplate = options?.scopedInTemplate ?? true
-  _options.extend = options?.extend ?? {}
-  _options.specialEnvironment = options?.specialEnvironment ?? 'vite'
-  _options.applyAutoprefixer = options?.applyAutoprefixer ?? true
-  _options.simplifyTitles = options?.simplifyTitles ?? false
-  _options.forceIgnoreClassLineInTitles = options?.forceIgnoreClassLineInTitles ?? false
-  _options.forceHydratedTemplate = options?.forceHydratedTemplate ?? false
-
-  return _options
+  return {
+    defines: options?.defines ?? [],
+    jsx: options?.jsx ?? false,
+    jsxIgnoreEntryFile: options?.jsxIgnoreEntryFile ?? true,
+    scopedInTemplate: options?.scopedInTemplate ?? true,
+    extend: options?.extend ?? {},
+    specialEnvironment: options?.specialEnvironment ?? 'vite',
+    applyAutoprefixer: options?.applyAutoprefixer ?? true,
+    simplifyTitles: options?.simplifyTitles ?? false,
+    forceIgnoreClassLineInTitles: options?.forceIgnoreClassLineInTitles ?? false,
+    forceHydratedTemplate: options?.forceHydratedTemplate ?? false,
+  } as const satisfies UnunuraOptions
 }
