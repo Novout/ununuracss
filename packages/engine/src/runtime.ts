5413f42be4c13f58ba368063377e3f6db03aa602
Giovane Cardoso <novout@hotmail.com>
2023-02-27 18:55:30 -0300

    chore: base runtime in engine

---
+++ packages/engine/src/runtime.ts
@@ -0,0 +1,112 @@
+import { resolvedViteOptions } from 'ununura'
+import { UnunuraOptions, UNUNURA_FLAG } from 'ununura-shared'
+import { getGlobalReset } from './globals'
+import { lexToRawTitles } from './lexer'
+import { nodesToCSS } from './pipeline'
+import { purgeCSS } from './purge'
+
+export const initRuntime = (options: UnunuraOptions) => {
+  if (typeof window == 'undefined') return
+
+  const defDocument = document
+
+  let injectStyle = defDocument.createElement('style')
+
+  const ununura = resolvedViteOptions(options)
+
+  const getTemplate = () => {
+    return defDocument.body && defDocument.body.outerHTML
+  }
+
+  const getStyle = () => {
+    if (!defDocument.querySelector(`#${UNUNURA_FLAG}`)) {
+      injectStyle = defDocument.createElement('style')
+      injectStyle.id = UNUNURA_FLAG
+      defDocument.documentElement.prepend(injectStyle)
+    }
+
+    return injectStyle
+  }
+
+  const setStyle = (newCss: string) => {
+    const rawCss = getStyle().innerHTML
+
+    setTimeout(() => {
+      const _style = getStyle()
+
+      const normalizedCss = purgeCSS(rawCss + newCss, true)
+
+      _style.innerHTML = normalizedCss
+    }, 0)
+  }
+
+  const generate = (targets: Element[]) => {
+    const template = getTemplate()
+
+    if (!template || targets.length === 0) return
+
+    const nodes = targets.map((target, index) => ({
+      class: target.className,
+      tag: target.tagName,
+      position: {
+        // one-file alternative for scoped items using index key
+        start: { line: index, column: index, offset: index },
+        end: { line: index, column: index, offset: index },
+      },
+    }))
+
+    let { css, titles } = nodesToCSS(nodes, template, UNUNURA_FLAG, ununura)
+
+    if (css.length === 0) return
+
+    targets.forEach((target) => {
+      const classes = lexToRawTitles(target.className)
+
+      classes?.forEach((cl) => {
+        titles.forEach(([prev, set]) => {
+          if (cl === prev) {
+            target.className = target.className.replace(prev, set)
+          }
+        })
+      })
+    })
+
+    const globalResetRaw = getGlobalReset(targets?.map((t) => t.className).join(' ') ?? '')
+    const bufferRaw = css.reduce((acc, css) => (acc += `${css}\n`))
+    setStyle(`${globalResetRaw ? `\n${globalResetRaw}` : ''}${bufferRaw}`)
+  }
+
+  const MO = new MutationObserver((list, _observer) => {
+    const targets: Element[] = []
+
+    list.forEach((item) => {
+      if (item.type === 'childList' && item.target !== injectStyle) {
+        item.addedNodes.forEach((node) => {
+          if (!node) return
+
+          const _node = node as Element
+
+          if (!_node?.className) return
+
+          targets.push(_node)
+        })
+      } else if (item.type === 'attributes') {
+        if (!item.target) return
+
+        const _node = item.target as Element
+
+        if (!_node?.className) return
+
+        targets.push(_node)
+      }
+    })
+
+    generate(targets)
+  })
+
+  MO.observe(defDocument.documentElement || window.document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+  })
+}
--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,6 +1,6 @@
 import { resolvedViteOptions } from 'ununura'
-import { getGlobalReset, lexToRawTitles, nodesToCSS, purgeCSS } from 'ununura-engine'
-import { UnunuraResolvableOptions, UNUNURA_FLAG } from 'ununura-shared'
+import { initRuntime } from 'ununura-engine'
+import { UnunuraResolvableOptions } from 'ununura-shared'
 
 declare global {
   interface Window {
@@ -9,112 +9,10 @@ declare global {
 }
 
 ;(() => {
-  if (typeof window == 'undefined') return
-
-  const defDocument = document
-
-  let injectStyle = defDocument.createElement('style')
-
   const ununura = resolvedViteOptions(window.__UNUNURA__ ?? {})
   ununura.applyAutoprefixer = false
   ununura.scopedInTemplate = false
   ununura.specialEnvironment = 'runtime'
 
-  const getTemplate = () => {
-    return defDocument.body && defDocument.body.outerHTML
-  }
-
-  const getStyle = () => {
-    if (!defDocument.querySelector(`#${UNUNURA_FLAG}`)) {
-      injectStyle = defDocument.createElement('style')
-      injectStyle.id = UNUNURA_FLAG
-      defDocument.documentElement.prepend(injectStyle)
-    }
-
-    return injectStyle
-  }
-
-  const setStyle = (newCss: string) => {
-    const rawCss = getStyle().innerHTML
-
-    setTimeout(() => {
-      const _style = getStyle()
-
-      const normalizedCss = purgeCSS(rawCss + newCss, true)
-
-      _style.innerHTML = normalizedCss
-    }, 0)
-  }
-
-  const generate = (targets: Element[]) => {
-    const template = getTemplate()
-
-    if (!template || targets.length === 0) return
-
-    const nodes = targets.map((target, index) => ({
-      class: target.className,
-      tag: target.tagName,
-      position: {
-        // one-file alternative for scoped items using index key
-        start: { line: index, column: index, offset: index },
-        end: { line: index, column: index, offset: index },
-      },
-    }))
-
-    let { css, titles } = nodesToCSS(nodes, template, UNUNURA_FLAG, ununura)
-
-    if (css.length === 0) return
-
-    targets.forEach((target) => {
-      const classes = lexToRawTitles(target.className)
-
-      classes?.forEach((cl) => {
-        titles.forEach(([prev, set]) => {
-          if (cl === prev) {
-            target.className = target.className.replace(prev, set)
-          }
-        })
-      })
-    })
-
-    const globalResetRaw = getGlobalReset(targets?.map((t) => t.className).join(' ') ?? '')
-    const bufferRaw = css.reduce((acc, css) => (acc += `${css}\n`))
-    setStyle(`${globalResetRaw ? `\n${globalResetRaw}` : ''}${bufferRaw}`)
-  }
-
-  const MO = new MutationObserver((list, _observer) => {
-    const targets: Element[] = []
-
-    list.forEach((item) => {
-      if (item.type === 'childList' && item.target !== injectStyle) {
-        item.addedNodes.forEach((node) => {
-          if (!node) return
-
-          const _node = node as Element
-
-          if (!_node?.className) return
-
-          targets.push(_node)
-        })
-      } else if (item.type === 'attributes') {
-        if (!item.target) return
-
-        const _node = item.target as Element
-
-        if (!_node?.className) return
-
-        targets.push(_node)
-      }
-    })
-
-    generate(targets)
-  })
-
-  setTimeout(() => {
-    MO.observe(defDocument.documentElement || window.document.body, {
-      childList: true,
-      subtree: true,
-      attributes: true,
-    })
-  }, 0)
+  initRuntime(ununura)
 })()
