893a7c2afdb4de7eeea1b889e9dca123acfa010a
Novout <novout@hotmail.com>
2023-03-16 17:48:06 -0300

    chore: typo

--- examples/vue3/vite.config.ts
+++ examples/vue3/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { ununura } from '../../packages/plugin-vite/dist'
+import { ununura } from 'vite-plugin-ununura'
 import inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
